545e7b075339a6ee937550db6889f1c5a034e001
andreyrunov <elefant86@bk.ru>
2023-01-26 16:06:21 +0300

    add vacancies component

--- components/index.ts
+++ components/index.ts
@@ -3,3 +3,5 @@ export * from './Tags/Button/Button';
 export * from './Tags/P/P';
 export * from './Tags/Rating/Rating';
 export * from './Labels/Label';
+export * from './Card/Card';
+export * from './HhData/HhData';
---
+++ pages/search.tsx
@@ -0,0 +1,40 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next';
+import React from 'react';
+import { withLayout } from '../layout/Layout';
+import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
+import { ParsedUrlQuery } from 'querystring';
+
+function Search(): JSX.Element {
+
+    return (
+        <>
+            Search
+        </>
+    );
+}
+
+
+export default withLayout(Search);
+
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+
+    const firstCategory = 0;
+    const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+        firstCategory
+    });
+
+
+    return {
+        props: {
+            menu,
+            firstCategory,
+        }
+    };
+};
+
+interface HomeProps extends Record<string, unknown> {
+    menu: MenuItem[];
+    firstCategory: number;
+}
