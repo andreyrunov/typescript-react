f3b8b9d48c6c675fb35cfa1f517af4b20880de8f
andreyrunov <elefant86@bk.ru>
2023-01-28 15:41:03 +0300

    add search component with handler

--- components/index.ts
+++ components/index.ts
@@ -3,6 +3,7 @@ export * from './Tags/Input/Input';
 export * from './Tags/Textarea/Textarea';
 export * from './Tags/Button/Button';
 export * from './Tags/P/P';
+export * from './Tags/Search/Search';
 export * from './Tags/Sort/Sort';
 export * from './Tags/Rating/Rating';
 export * from './Labels/Label';
