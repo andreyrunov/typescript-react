61fe0d335893b29d18384035118b5b8f7226060b
andreyrunov <elefant86@bk.ru>
2023-01-31 18:06:05 +0300

    add axios post query from form to test backend

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReviewFormProps } from './ReviewForm.props';
 import styles from './ReviewForm.module.css';
 import cn from 'classnames';
@@ -8,29 +8,42 @@ import { Rating } from '../Tags/Rating/Rating';
 import { Textarea } from '../Tags/Textarea/Textarea';
 import { Button } from '../Tags/Button/Button';
 import { useForm, Controller } from 'react-hook-form';
-import { IReviewForm } from './ReviewForm.interface';
-
+import { IReviewForm, IReviewSentResponse } from './ReviewForm.interface';
+import axios from 'axios';
+import { API } from '../../helpers/api';
 
 export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps): JSX.Element => {
-    const { register, control, handleSubmit, formState: { errors } } = useForm<IReviewForm>();
+    const { register, control, handleSubmit, formState: { errors }, reset } = useForm<IReviewForm>();
+    const [isSuccess, setIsSuccess] = useState<boolean>(false);
+    const [error, setError] = useState<string>();
 
-    const onSubmit = (data: IReviewForm) => {
-        console.log(data);
+    const onSubmit = async (formData: IReviewForm) => {
+        try {
+            const { data } = await axios.post<IReviewSentResponse>(API.review.createDemo, { ...formData, productId });
+            if (data.message) {
+                setIsSuccess(true);
+                reset();
+            } else {
+                setError('Что-то пошло не так');
+            }
+        } catch (e: any) {
+            setError(e.message);
+        }
     };
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <div className={cn(styles.reviewForm, className)} {...props}>
                 {/* в этом инпуте используем синтаксис неуправляемых компонентов (register) */}
-                <Input 
-                    {...register('name', { required: { value: true, message: 'Заполните ваше имя' }, maxLength: {value: 5, message: 'Длинна не более 5 символов'}})} 
-                    className={styles.inputName} 
+                <Input
+                    {...register('name', { required: { value: true, message: 'Заполните ваше имя' }, maxLength: { value: 5, message: 'Длинна не более 5 символов' } })}
+                    className={styles.inputName}
                     placeholder='Имя'
                     error={errors.name}
                 />
-                <Input 
-                    {...register('title', { required: { value: true, message: 'Заполните заголовок отзыва' }, maxLength: {value: 25, message: 'Длинна не более 25 символов'}})}
-                    className={styles.inputHeader} 
+                <Input
+                    {...register('title', { required: { value: true, message: 'Заполните заголовок отзыва' }, maxLength: { value: 25, message: 'Длинна не более 25 символов' } })}
+                    className={styles.inputHeader}
                     placeholder='Заголовок отзыва'
                     error={errors.title}
                 />
@@ -40,24 +53,24 @@ export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps):
                         control={control}
                         name='rating'
                         // field позволяет управлять состоянием рейтинга
-                        rules={{ required: { value: true, message: 'Укажите рейтинг' }}}
+                        rules={{ required: { value: true, message: 'Укажите рейтинг' } }}
                         render={({ field }) => (
                             // в рейтинге используем синтаксис управляемых компонентов, поэтому оборачиваем его Controller'ом
-                            <Rating 
-                                isEditable 
-                                rating={field.value} 
-                                ref={field.ref} 
+                            <Rating
+                                isEditable
+                                rating={field.value}
+                                ref={field.ref}
                                 setRating={field.onChange}
-                                error={errors.rating} 
+                                error={errors.rating}
                             />
                         )}
                     />
 
                 </div>
 
-                <Textarea 
-                    {...register('description', { required: { value: true, message: 'Заполните текст отзыва' }, maxLength: { value: 250, message: 'Длинна не более 250 символов' } })} 
-                    placeholder='Текст отзыва' 
+                <Textarea
+                    {...register('description', { required: { value: true, message: 'Заполните текст отзыва' }, maxLength: { value: 250, message: 'Длинна не более 250 символов' } })}
+                    placeholder='Текст отзыва'
                     className={styles.description}
                     error={errors.description}
                 />
@@ -67,11 +80,15 @@ export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps):
 
                 </div>
             </div>
-            <div className={styles.success}>
+            {isSuccess && <div className={cn(styles.success, styles.panel)}>
                 <div className={styles.successTitle}>Ваш отзыв отправлен</div>
                 <div className={styles.successDescription}>Спасибо! Ваш отзыв будет опубликован после проверки</div>
-                <CloseIcon className={styles.close} />
-            </div>
+                <CloseIcon className={styles.close} onClick={() => setIsSuccess(false)} />
+            </div>}
+            {error && <div className={cn(styles.error, styles.panel)}>
+                Что-то пошло не так, попробуйте обновить страницу и отправить повторно отзыв
+                <CloseIcon className={styles.close} onClick={() => setError(undefined)} />
+            </div>}
         </form>
     );
 };
