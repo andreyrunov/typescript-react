c2c14c6f056b89b455f557fe4ffcce85ef0dbdcb
andreyrunov <elefant86@bk.ru>
2023-02-02 15:13:08 +0300

    add motion to sort

--- components/Tags/Product/Product.tsx
+++ components/Tags/Product/Product.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { ForwardedRef, forwardRef, useRef, useState } from 'react';
 import { ProductProps } from './Product.props';
 import styles from './Product.module.css';
 import cn from 'classnames';
@@ -11,8 +11,9 @@ import { Divider } from '../Divider/Divider';
 import Image from 'next/image';
 import { Review } from '../../Review/Review';
 import { ReviewForm } from '../../ReviewForm/ReviewForm';
+import { motion } from 'framer-motion';
 
-export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
+export const Product = motion(forwardRef(({ product, className, ...props }: ProductProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
     const [isReviewOpened, setIsReviewOpened] = useState<boolean>(false);
     const reviewRef = useRef<HTMLDivElement>(null);
     
@@ -22,10 +23,10 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
             behavior: 'smooth',
             block: 'start'
         });
-    }
+    };
 
     return (
-        <div className={className} {...props}>
+        <div className={className} {...props} ref={ref}>
 
             <Card className={styles.product}>
                 <div className={styles.logo}>
@@ -103,4 +104,4 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
             </Card>
         </div>
     );
-};
+}));
